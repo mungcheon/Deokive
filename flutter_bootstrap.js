9f43d8402291eb40fcffe8de306c23454ea72e95
코드랑 싸우는 멍천 <dmstj5308@naver.com>
2026-07-19 08:34:03 +0900

    deploy: normalize animation goods categories

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1683557370" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1668814468" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
